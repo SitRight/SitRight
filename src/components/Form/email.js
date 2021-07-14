3dfd02ca6ded2dc337c70910f91d83f194268d2d
ceo.sitright.com <ceo.sitright@gmail.com>
2021-07-13 19:15:07 -0500

    email functionality

--- src/components/Form/email.js
+++ src/components/Form/email.js
@@ -1,16 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import TextField from '@material-ui/core/TextField';
 //import { makeStyles } from '@material-ui/core/styles';
 
 
+
 const Email = () => {
+  const [data, setData] = useState({
+    email: ""
+  });
+
+  const { email } =  data;
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.email]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("https://v1.nocodeapi.com/sitright/google_sheets/brlkDKHNfTNVAngx?tabId=Sheet1", {
+        method: "POST",
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: JSON.stringify([email, new Date().toLocaleString])
+        }
+      );
+      await response.json()
+      setData({...data, email:''})
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
     return (
-        <TextField
-          id="outlined-helperText"
-          label="Email"
-          variant="outlined"
-        >
-        </TextField>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="outlined-helperText"
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={handleChange}
+          >
+          </TextField>
+        </form>
     )
 }
   
